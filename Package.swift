840fb46ff5edae7fadb64bd0391b709362de185e
Carlos Fonseca <carlosefonseca@gmail.com>
2024-10-07 11:34:40 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "RxGoogleMaps",
+  platforms: [.iOS(.v15)],
   products: [
     .library(name: "RxGoogleMaps", targets: ["RxGoogleMaps"]),
   ],
